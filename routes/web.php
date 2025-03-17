cdb5fed93f5b9409e8363591822bb809f7405faf
Rafiadi28 <rafiadi28@gmail.com>
2025-03-17 12:07:23 +0700

    Role-Permision

---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/',[AuthController::class, 'login'])->name('login');
